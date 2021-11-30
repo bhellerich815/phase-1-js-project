ca430364dcf5b0d9924b135bc9acc652847eb8b0
Brooke Hellerich <brookeh815@cox.net>
2021-11-30 14:26:33 -0600

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# dog adoption
+# Dog Liker
+## Overview
+### This app is designed to randomly show you dog photos, and you will be able to like or dislike the photo. This should raise your awareness of different breeds of dogs. Underneath each photo, you should see a breed name and short description of the dog breed.
 
-## overview
-
-
-### features
+## Features
+### This app has 3 features:
+#### 1: Show photos of dog breeds
+#### 2: Allow you to like or dislike photos
+#### 3: Give a breed name and short description of the dog in the photo
 
 #### How to Use
-##### Scroll through dogs - you have the option to view a video of each adoptable dog, as well as like their profile card or learn more about adopting the dog. In order to learn more about adopting each dog, simply type your email address into the space provided, and hit "adopt dog". Pressing the "like" button will return "I Like This Dog!" 
+##### 
--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 //Event Listeners
-document.querySelector('#animal-form').addEventListener('submit', handleSubmit)
+// document.querySelector('#animal-form').addEventListener('submit', handleSubmit)
 
 //Event Handlers
-function handleSubmit(e){
-    e.preventDefault()
-    console.log(e.target)
-    let dogObject = {
-        name:e.target.name.value,
-        imageURL:e.target.image_url.value,
-        breedGroup:e.target.breed_group.value,
-        bredFor:e.target.bred_for.value,
-        temperament:e.target.temperament.value,
-    }
-    renderOneDog(dogObject)
-}
+// function handleSubmit(e){
+//     e.preventDefault()
+//     console.log(e.target)
+//     let dogObject = {
+//         name:e.target.name.value,
+//         imageURL:e.target.image_url.value,
+//         breedGroup:e.target.breed_group.value,
+//         bredFor:e.target.bred_for.value,
+//         temperament:e.target.temperament.value,
+//     }
+//     renderOneDog(dogObject)
+// }
 
 //DOM Render Functions
 function renderOneDog(dog){
@@ -21,48 +21,61 @@ function renderOneDog(dog){
     let card = document.createElement('li')
     card.className = 'card'
     card.innerHTML = `
-         <video src="${dog.imageURL}">
-         <div class="content">
-            <h4>${dog.name}</h4>
-            <h5>${dog.breedGroup}</h5>
-            <h6>${dog.bredFor}</h6>
-            <h7>${dog.temperament}</h7>
-        </div>
+         <image src="${imageURL}">
         <div class= "buttons">
             <button> Like </button>
-            <button> Love </button>
+            <button> Dislike </button>
         </div>
     `
-
-//Add animal card to DOM
-document.querySelector('animal-list').appendChild(card)
+    //Add animal card to DOM
+    document.querySelector('animal-list').appendChild(card)
 }
 
 //Fetch requests
 //Get fetch for all dog resources
-function getAllDogs(){
-    var xAPIKey = 'fe4eff12-acfa-4723-8956-d003f09be2e1'
-    var url = 'https://api.thedogapi.com/v1/breeds'
-    return fetch (url,{
-        method: 'GET',
-        headers: {
-            "Authorization": xAPIKey,
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(res => res.json())
-    .then(animalData => animalData.forEach(dog => renderOneDog(dog)))
-    .catch(error => console.log)
-}
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("x-api-key", "fe4eff12-acfa-4723-8956-d003f09be2e1")
+
+// var formData = new FormData();
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    // body: formdata,
+    redirect: 'follow'
+};
+
+fetch("https://api.thedogapi.com/v1/images/search", requestOptions)
+    .then(response => response.text())
+    .then(res => res.json)
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+// function getAllDogs(){
+//     var xAPIKey = 'fe4eff12-acfa-4723-8956-d003f09be2e1'
+//     var url = 'https://api.thedogapi.com/v1/images/search'
+//     return fetch (url,{
+//         method: 'GET',
+//         headers: {
+//             "Authorization": xAPIKey,
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(res => res.json())
+//     .then(animalData => animalData.forEach(dog => renderOneDog(dog)))
+//     .catch(error => console.log)
+
 
 //Initial Render
 //Get data and render dogs to the DOM
-function initialize(){
-    getAllDogs()
-    // animalData.forEach(dog => renderOneDog(dog))
-}
+// function initialize(){
+//     getAllDogs()
+//     animalData.forEach(dog => renderOneDog(dog))
+// }
 // initialize()
 
 //Button Responses
-document.querySelector('#like').addEventListener('click', () => like('I like this dog'))
-document.querySelector('#love').addEventListener('click', () => love('I love this dog'))
+// document.querySelector('#like').addEventListener('click', () => like('I like this dog'))
+// document.querySelector('#dislike').addEventListener('click', () => dislike('I dislike this dog'))
